ac32483b0611e6d0e85ad5b0fb66c11e9118b54a
Zachariah Gibbens <zach@gibbens.dev>
2022-01-11 17:08:56 -0400

    new file:   .gitignore 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 date -u >> /root/test.txt
+<<<<<<< HEAD
+<<<<<<< HEAD
+apt install -y curl sudo
+useradd -m -p changeme user
+adduser user sudo
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.orig
+echo "deb https://deb.debian.org/debian/ experimental main contrib non-free
+deb-src https://deb.debian.org/debian/ experimental main contrib non-free
+deb https://deb.debian.org/debian/ sid main contrib non-free
+deb-src https://deb.debian.org/debian/ sid main contrib non-free" | sudo tee /etc/apt/sources.list
+sudo apt update
+mkdir /home/user/.ssh/
+curl https://github.com/zachgibbens.keys >> /home/user/.ssh/authorized_keys
+chown user:user /home/user/.ssh
+chown user:user /home/user/.ssh/authorized_keys
+chmod 600 /home/user/.ssh/authorized_keys
+chmod 700 /home/user/.ssh
+sudo apt install -y mosh screen ssh
+=======
+>>>>>>> 8e8df36 (	new file:   install.sh)
+=======
+>>>>>>> 8e8df36 (	new file:   install.sh)
